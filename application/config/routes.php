7f64362ee8612b7612b7b249764a4b97e1b373bd
Walid Algazali <walidalgazali14@gmail.com>
2021-08-26 13:53:07 +0700

    Update Routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,11 @@
 $route['default_controller'] 	= "dashboard";
 $route['404_override'] 			= '';
 $route['dashboard'] 	= "dashboard";
+$route['table'] 	= "table";
+$route['profile'] 	= "profile";
+$route['basic'] 	= "basic";
+$route['icon'] 	= "icon";
+$route['blank'] 	= "blank";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
